c62bd24114c0612bb82dc3f192207ebd22447562
richad.97 <rcastillo2022@gmail.com>
2021-12-13 23:29:53 -0800

    completed assignment

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,15 +1,53 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import { render, screen, waitFor} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import Display from './../Display';
+import Display from "./../Display";
 
+import mockFetchShow from "./../../api/fetchShow";
+jest.mock("./../../api/fetchShow");
 
-test('renders without errors with no props', ()=>{
+const testShow = {
+  name: "test show",
+  summary: "test summary",
+  seasons: [
+    {
+      id: 0,
+      name: "Season 1",
+      episoddes: [],
+    },
+    {
+      id: 1,
+      name: "Season 2",
+      episoddes: [],
+    },
+  ],
+};
 
+test("renders without errors with no props", () => {
+  render(<Display />);
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test("renders Show component when the button is clicked ", async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow);
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+  render(<Display />);
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+  const show = await screen.findByTestId("show-container");
+  expect(show).toBeInTheDocument();
+});
+
+test("renders show season options matching your data when the button is clicked", async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow);
+  const displayFunc = jest.fn();
+
+  render(<Display displayFunc={displayFunc} />);
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  await waitFor(() => {
+    expect(displayFunc).toHaveBeenCalled();
+  });
+});
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import { render, screen } from '@testing-library/react';
-import Episode from './../Episode';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import Episode from "./../Episode";
 
+const testEpisode = {
+  id: [],
+  name: "",
+  image:
+    "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
+  season: 1,
+  number: 1,
+  summary: "test summary",
+  runtime: 1,
+};
 
-test("renders without error", () => {});
+const testEpisodeNoImage = {
+  id: [],
+  name: "",
+  season: 1,
+  number: 1,
+  summary: "test summary",
+  runtime: 1,
+};
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders without error", () => {
+  render(<Episode episode={testEpisode} />);
+});
 
-test("renders default image when image is not defined", ()=>{});
+test("renders the summary test passed as prop", () => {
+  render(<Episode episode={testEpisode} />);
+  const summary = screen.queryByText(/test summary/i);
+  expect(summary).toBeInTheDocument();
+  expect(summary).toBeTruthy();
+});
+
+test("renders default image when image is not defined", () => {
+  render(<Episode episode={testEpisodeNoImage} />);
+  const image = screen.queryByAltText("./stranger_things.png");
+});
